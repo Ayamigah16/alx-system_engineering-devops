bf65f203fc76fd5a05cef56ff429d462b3e87a28
Abraham Ayamigah <abkc070@gmail.com>
2023-11-02 16:56:14 -0700

    bug fixee

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
 
-# Check if the argument contains "hbtn" followed by one or more "n" characters and print the result
+# Check if the argument contains "hbtn" followed
+# by one or more "n" characters and print the result
 puts ARGV[0].scan(/hbt+n/).join
--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
 
-# Check if the argument starts with "hb" followed by one or more "t" characters and print the result
+# Check if the argument starts with "hb" followed
+# by one or more "t" characters and print the result
 puts ARGV[0].scan(/hb+t/).join
