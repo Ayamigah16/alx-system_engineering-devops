57f19a38aa132a52f7e4a0c5935b0c71e65edff1
Abraham Ayamigah <abkc070@gmail.com>
2023-11-02 17:23:44 -0700

    bug fixed for task 1 and 2

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 
-# Check if the argument contains "hbt" followed by 0 to 5 characters and then "n" and print the result
-puts ARGV[0].scan(/hbt.{0,5}n/).join
+# Check if the argument contains "hbt" followed by 2 to 5 characters and then "n" and print the result
+puts ARGV[0].scan(/hbt.{2,5}n/).join
--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
 
 # Check if the argument starts with "hb" followed
-# by one or more "t" characters and print the result
-puts ARGV[0].scan(/hb+t/).join
+# by zero or one occurence of "t" character and print the result
+puts ARGV[0].scan(/hb?tn/).join
