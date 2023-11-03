3e919e4584da31e9ab686ca520891618a2a294fc
Abraham Ayamigah <abkc070@gmail.com>
2023-11-02 17:41:46 -0700

    3. Repetition Token #2

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
 
-# Search for "hbt" followed by one or more "n" characters and print the result.
-puts ARGV[0].scan(/hbt+n/).join
+# Search for "hbt" followed by one or more
+# "n" characters and print the result.
+puts ARGV[0].scan(/hbt{1,4}n/).join
