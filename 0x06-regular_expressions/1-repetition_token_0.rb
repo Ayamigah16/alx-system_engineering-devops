680b0a5d94191cfe5bf4df1584046a1b4ef2647d
Abraham Ayamigah <abkc070@gmail.com>
2023-11-02 17:06:11 -0700

    trying to fix bugs

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/env ruby
 
-# Check if the argument contains "hbtn" followed
-# by one or more "n" characters and print the result
-puts ARGV[0].scan(/hbt+n/).join
+# Check if the argument contains "hbt" followed by 0 to 5 characters and then "n" and print the result
+puts ARGV[0].scan(/hbt.{0,5}n/).join
