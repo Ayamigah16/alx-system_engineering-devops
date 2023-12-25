522c21e2b7e58089340c49b5b0bc71c38f8614fd
Abraham Ayamigah <abrahamayamigah070@gmail.com>
2023-12-24 19:35:47 -0800

    Tasks done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,3 +5,8 @@ package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
+
+package { 'werkzeug':
+  ensure   => '2.1.1',  # or the version compatible with Flask 2.1.0
+  provider => 'pip3',
+}
