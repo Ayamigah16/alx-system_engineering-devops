0a2161e7d0a772e8922a9c2622f526f502bb700a
Abraham Ayamigah <abrahamayamigah070@gmail.com>
2023-12-26 04:13:32 -0800

    ssh tasks done

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,31 @@
+# 100-puppet_ssh_config.pp
+
+# Ensure the SSH client configuration directory exists
+file { '/home/root/.ssh':
+  ensure  => 'directory',
+  mode    => '0700',
+  owner   => 'root',
+  group   => 'root',
+}
+
+# Ensure the SSH client configuration file exists
+file { '/home/root/.ssh/config':
+  ensure  => 'file',
+  mode    => '0600',
+  owner   => 'root',
+  group   => 'root',
+  content => template('./ssh_config.erb'),
+}
+
+# Disable password authentication and set the identity file in the SSH client configuration
+file_line { 'Turn off passwd auth':
+  path    => '/home/root/.ssh/config',
+  line    => 'PasswordAuthentication no',
+  match   => '^#?PasswordAuthentication',
+}
+
+file_line { 'Declare identity file':
+  path    => '/home/root/.ssh/config',
+  line    => 'IdentityFile ~/.ssh/school',
+  match   => '^#?IdentityFile',
+}
