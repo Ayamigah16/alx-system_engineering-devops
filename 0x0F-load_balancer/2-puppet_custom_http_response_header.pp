ce7bc64d0df2460b9c6b05b611194acb3a478cd3
Abraham Ayamigah <abkc070@gmail.com>
2024-03-12 10:42:56 -0700

    task 2 updated

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,42 +1,25 @@
-# 2-puppet_custom_http_response_header.pp
+# Installs a Nginx server with custom HTTP header
 
-# Define a class for configuring the custom HTTP header
-class custom_http_response_header {
-  
-  # Ensure nginx is installed
-  package { 'nginx':
-    ensure => installed,
-  }
-
-  # Define a custom nginx location block to add the custom header
-  file { '/etc/nginx/sites-available/default':
-    ensure  => present,
-    content => "
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-
-    root /var/www/html;
-    index index.html index.htm index.nginx-debian.html;
-
-    server_name _;
-
-    location / {
-        add_header X-Served-By $hostname;
-        try_files $uri $uri/ =404;
-    }
-}
-    ",
-    notify  => Service['nginx'],
-  }
-
-  # Enable and start nginx service
-  service { 'nginx':
-    ensure  => running,
-    enable  => true,
-  }
-
+exec {'update':
+  provider => shell,
+  command  => 'sudo apt-get -y update',
+  before   => Exec['install Nginx'],
 }
 
-# Include the custom class
-include custom_http_response_header
+exec {'install Nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+  before   => Exec['add_header'],
+}
+
+exec { 'add_header':
+  provider    => shell,
+  environment => ["HOST=${hostname}"],
+  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+  before      => Exec['restart Nginx'],
+}
+
+exec { 'restart Nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
