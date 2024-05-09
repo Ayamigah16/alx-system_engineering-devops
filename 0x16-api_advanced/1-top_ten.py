1a24115e6f1d3a1d98d7c4dff986d791318cecc3
Abraham Ayamigah <abkc070@gmail.com>
2024-05-09 13:24:50 -0700

    1. Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+1-top_ten
+"""
+import requests
+
+def top_ten(subreddit):
+    """Prints the titles of the first 10 hot posts for a given subreddit"""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        posts = data["data"]["children"]
+        if not posts:
+            print("No posts found for this subreddit.")
+            return
+        for post in posts:
+            print(post["data"]["title"])
+    else:
+        print(None)
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        top_ten(sys.argv[1])
